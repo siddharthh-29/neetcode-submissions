08a664b4bc2e5778d751fd625779aa1ecc1b1d35
Siddharth Sivapuram <sivapuramsiddharth29@gmail.com>
2026-09-24 21:32:53 +0530

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(int i=0;i<=nums.size();i++)if(mp.find(i)==mp.end())return i;
+        return 0;
+    }
+};
