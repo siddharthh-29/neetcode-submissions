e1a81ee2475ff59f42c7250d6a5c47dced009a4c
Siddharth Sivapuram <sivapuramsiddharth29@gmail.com>
2026-09-25 16:31:08 +0530

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sqr(int n){
+        int sum=0;
+        while(n){
+            int digit=n%10;
+            sum+=(digit*digit);
+            n=n/10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        set<int>st;
+        while(true){
+            if(n==1)return true;
+            else if(st.find(n)!=st.end())return false;
+            st.insert(n);
+            n=sqr(n);
+        }
+        return false;
+    }
+};
